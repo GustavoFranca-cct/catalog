114b9f93c8786bd66eb511ec133abdbf3ca36a83
Gustavo França <2024533@student.cct.ie>
2026-07-08 13:27:25 +0100

    Add BookView API test and update requirements; configure pytest settings

--- api/views.py
+++ api/views.py
@@ -1,3 +1,4 @@
+from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
@@ -6,6 +7,6 @@ class BookView(APIView):
     def get(self, request, *args, **kwargs):
         return Response({
             "hello": "django"
-        })
+        }, status=status.HTTP_200_OK)
 
 book_view = BookView.as_view()
