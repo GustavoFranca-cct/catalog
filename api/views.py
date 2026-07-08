a24cf544aa952227eb1091c623282532647439db
Gustavo França <2024533@student.cct.ie>
2026-07-08 15:37:43 +0100

    Implement Book model, serializer, and API view for retrieving all books

--- api/views.py
+++ api/views.py
@@ -1,12 +1,13 @@
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
-
+from .models import Book
+from .serializers import BookSerializer
 
 class BookView(APIView):
     def get(self, request, *args, **kwargs):
-        return Response({
-            "hello": "django"
-        }, status=status.HTTP_200_OK)
+        all_books = Book.objects.all()
+        serializer = BookSerializer(all_books, many=True)
+        return Response(serializer.data)
 
 book_view = BookView.as_view()
