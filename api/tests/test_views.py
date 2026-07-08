ef28b1e70f50fdf2dd334364c7ef8515226ba96c
Gustavo França <2024533@student.cct.ie>
2026-07-08 18:09:53 +0100

    Add POST method to BookView for creating new book entries

--- api/tests/test_views.py
+++ api/tests/test_views.py
@@ -1,8 +1,21 @@
 from rest_framework.test import APITestCase
 from django.urls import reverse
+from api.models import Book
 
 class BookViewTest(APITestCase):
+
     def test_response_is_correct(self):
+        book = Book.objects.create(
+            title='Test Book', 
+            author='Author Name',
+            description='A test book description.'
+            )
+
         url = reverse('api:books')
         response = self.client.get(url, format='json')
         assert response.status_code == 200
+        first_book = response.data[0]
+        assert first_book['title'] == book.title
+        assert first_book['author'] == book.author
+        assert first_book['description'] == book.description
+      
--- api/views.py
+++ api/views.py
@@ -10,4 +10,11 @@ class BookView(APIView):
         serializer = BookSerializer(all_books, many=True)
         return Response(serializer.data)
 
+    def post(self, request, *args, **kwargs):
+        serializer = BookSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 book_view = BookView.as_view()
